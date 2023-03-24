a00706b0fe834ce8dd19c87b0cb974375a597814
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2023-03-24 17:08:50 +0100

    default code-blocks to console highlighting

--- esrum/source/conf.py
+++ esrum/source/conf.py
@@ -18,6 +18,8 @@ extensions = []
 templates_path = ["_templates"]
 exclude_patterns = []
 
+# Default language for code blocks (terminal sessions)
+highlight_language = "console"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
